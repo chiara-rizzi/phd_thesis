ef15c98d724d5a77b825ca5545a5be06393b0ee4
chiara-rizzi <chiara_rizzi@hotmail.it>
2017-12-19 12:00:27 +0100

    add macro to make toys

--- produce_plots/stat/poisson.py
+++ produce_plots/stat/poisson.py
@@ -8,7 +8,9 @@ c.cd()
 
 gStyle.SetOptTitle(0)
 
-fb = TF1("fb","TMath::Poisson(x,50)",0,125);
+tc = 65
+
+fb = TF1("fb","TMath::Poisson(x,50)",25,125);
 fb.SetLineColor(kRed)
 #fb.SetFillColorAlpha(kRed, 0.5)
 fb.SetFillColor(6)
@@ -20,15 +22,18 @@ fb.GetXaxis().SetTickLength(0)
 fb.GetYaxis().SetTickLength(0)
 fb.Draw("F")
 
-fs = TF1("fs","TMath::Poisson(x,75)",0,125);
+fs = TF1("fs","TMath::Poisson(x,75)",25,125);
 fb.Draw()
 fs.SetLineColor(kBlue)
 fs.SetFillColorAlpha(kBlue, 0.5)
 fs.Draw("fc same")
 
-tcut = TLine(65, 0, 65, fb.GetMaximum())
+tcut = TLine(tc, 0, tc, fb.GetMaximum())
 tcut.SetLineStyle(3)
 tcut.Draw()
+
+t = TLatex()
+t.DrawLatex(tc - tc/30, -0.004, "t_{cut}")
 
 print fs.Integral(0,100)
 print fb.Integral(0,100)
