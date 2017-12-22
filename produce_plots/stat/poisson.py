e05f10a5fbf2ad24c1aeaed1851e651c2ec7b105
chiara-rizzi <chiara_rizzi@hotmail.it>
2017-12-22 09:44:24 +0100

    change plot

--- produce_plots/stat/poisson.py
+++ produce_plots/stat/poisson.py
@@ -6,11 +6,12 @@ print "ciao Chiara"
 c = TCanvas()
 c.cd()
 
-gStyle.SetOptTitle(0)
 
-tc = 75
+tc = 2
 
-fb = TF1("fb","TMath::Gaus(x,95,30,true)",0,150);
+#fb = TF1("fb","TMath::Gaus(x,95,30,true)",0,150);
+#fb = TF1("fb","Math::noncentral_chisquared_pdf(x,1,1)",0,15);
+fb = TF1("fb","ROOT::Math::noncentral_chisquared_pdf(x,1,15)",0,15);
 fb.SetLineColor(kRed)
 #fb.SetFillColorAlpha(kRed, 0.5)
 fb.GetXaxis().SetTitle("t")
@@ -25,31 +26,33 @@ fb.GetXaxis().SetTickLength(0)
 fb.GetYaxis().SetTickLength(0)
 
 
-fs = TF1("fs","TMath::Gaus(x,60,20,true)",0,150);
+#fs = TF1("fs","TMath::Gaus(x,60,20,true)",0,15);
+fs = TF1("fs","ROOT::Math::chisquared_pdf(x,1,0)", 0, 15);
 fs.SetLineColor(kBlue)
-mymax = max(fs.GetMaximum(), fb.GetMaximum())
-print mymax
-fb.SetMaximum(mymax*1.5)
+#mymax = max(fb.GetMaximum(), fb.GetMaximum())
+#print mymax
+fb.SetMaximum(2.5)
 fb.Draw("")
 fs.Draw("same")
 
-tcut = TLine(tc, 0, tc,  1.2*mymax)
+tcut = TLine(tc, 0, tc,  1.2)
 tcut.SetLineStyle(3)
 tcut.Draw()
 
 g_pmu = TGraph()
 g_pmu.SetPoint(0,tc,0)
-for i in [tc+step*0.1 for step in range(0, (151-tc)*10)]:
+for i in [tc+step*0.1 for step in range(0, (15-tc)*10)]:
     #print i
     g_pmu.SetPoint(g_pmu.GetN(), i, fs.Eval(i))
 g_pmu.SetFillColorAlpha(kBlue,0.5)
 g_pmu.Draw("f same")
 
 g_pb = TGraph()
-g_pb.SetPoint(0,tc,0)
-for i in [step*0.1 for step in range(0, (tc)*10)]:
+g_pb.SetPoint(g_pb.GetN(), 0, 0)
+for i in [step*0.01 for step in range(0, (tc)*100)]:
     #print i
     g_pb.SetPoint(g_pb.GetN(), i, fb.Eval(i))
+g_pb.SetPoint(g_pb.GetN(), tc, 0)
 g_pb.SetFillColorAlpha(kRed,0.5)
 g_pb.Draw("f same")
 
@@ -67,8 +70,7 @@ leg.SetLineWidth(0)
 
 leg.Draw()
 
-
-print fs.Integral(0,150)
-print fb.Integral(0,150)
+#print fs.Integral(0,15)
+#%print fb.Integral(0,15)
 
 c.SaveAs("pmu_pb.pdf")
