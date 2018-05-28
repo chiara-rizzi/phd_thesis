f84ac7a3b51e503e5d2a8a9f1b17f6f173142d45
chiara-rizzi <chiara_rizzi@hotmail.it>
2018-05-28 10:41:19 +0200

    updates sig bkg

--- produce_plots/stat/sig_bkg.py
+++ produce_plots/stat/sig_bkg.py
@@ -1,8 +1,8 @@
 import ROOT as r
+import math
 
 r.gStyle.SetOptStat(0)
 r.gStyle.SetOptTitle(0)
-
 
 fb = r.TFile.Open("/eos/atlas/user/c/crizzi/susy_multib/HFinputs_merged/SUSYHF_tag2.4.28-0-0/Bkg_2.4.28-0-0_skim_3b_with_wei.root","READ")
 fs = r.TFile.Open("/eos/atlas/user/c/crizzi/susy_multib/HFinputs_merged/SUSYHF_tag2.4.28-0-0/Sig_2.4.28-0-0_with_wei.root","READ")
@@ -17,7 +17,6 @@ if not ts:
 
 hb = r.TH1F("hb","hb",20,2000,4000)
 hs = r.TH1F("hs","hs",20,2000,4000)
-
 
 sel="(bjets_n>=3 && met>200)*(weight_mc*weight_lumi)*3600"
 tb.Draw("meff_incl>>hb",sel,"goff")
@@ -43,11 +42,10 @@ hb.GetYaxis().SetLabelSize(0)
 hb.GetXaxis().SetTickLength(0)
 hb.GetYaxis().SetTickLength(0)
 
-
 hb.Draw()
 hs.Draw("same")
 
-leg = r.TLegend(0.7,0.65,0.89,0.85)
+leg = r.TLegend(0.7,0.68,0.89,0.89)
 leg.AddEntry(hb,"Background","l")
 leg.AddEntry(hs,"Signal","l")
 leg.SetLineWidth(0)
@@ -88,6 +86,33 @@ tsr2.Draw()
 ar2 = r.TArrow(sr2 + 10 ,hb.GetMaximum()*0.5,sr2+200 -10, hb.GetMaximum()*0.5, 0.02)
 ar2.Draw()
 text.DrawLatex(sr2+20, hb.GetMaximum()*0.55, "SR2")
+
+s_sr = hs.Integral(9,10000)
+s_sr1 = hs.Integral(9,14)
+s_sr2 = hs.Integral(15,1000)
+
+b_sr = hb.Integral(9,10000)
+b_sr1 = hb.Integral(9,14)
+b_sr2 = hb.Integral(15,1000)
+
+print "Signal rates"
+print "SR:", s_sr
+print "SR1:", s_sr1
+print "SR2:", s_sr2
+
+print ""
+
+print "Background rates"
+print "SR:", b_sr
+print "SR1:", b_sr1
+print "SR2:", b_sr2
+
+print ""
+
+print "Significances"
+print "SR:", s_sr/math.sqrt(b_sr)
+print "SR1:", s_sr1/math.sqrt(b_sr1)
+print "SR2:", s_sr2/math.sqrt(b_sr2)
 
 
 c.Update()
